4789d3b1ec32d7caaf07f26e9babedab8984c3c3
Ashley <crashly0302@gmail.com>
2022-10-24 12:54:02 -0600

    Third push

--- src/Ch10.java
+++ src/Ch10.java
@@ -13,6 +13,7 @@ public class Ch10 {
             private int damage = 0;
 
             String[] armory = {"Exploding Pumpkin", "Candy Corn", "Paper Bag"}; //armory array
+
             public WarriorFred(String setNickName, int setLifeForce, int setDamage) { //constructor
                 this.nickName = setNickName; //explicit setters
                 this.lifeForce = setLifeForce;
@@ -22,13 +23,15 @@ public class Ch10 {
             public String getNickName() {
                 return nickName;
             }
+
             public void setNickName(String nickName) {
-               this.nickName = nickName;
+                this.nickName = nickName;
             }
 
             public int getLifeForce() {
                 return lifeForce;
             }
+
             public void setLifeForce(int lifeForce) {
                 this.lifeForce = lifeForce;
             }
@@ -36,20 +39,21 @@ public class Ch10 {
             public int getDamage() {
                 return damage;
             }
-            public void setDamage(int damage ) {
+
+            public void setDamage(int damage) {
                 this.damage = damage;
             }
 
 
-            public int takenDamage () { //random number for damage taken (1-10)
-              int[] damageDone = {1,2,3,4,5,6,7,8,9,10};
+            public int takenDamage() { //random number for damage taken (1-10)
+                int[] damageDone = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
                 Random random = new Random();
-                int newDamageDone=(damageDone[random.nextInt(damageDone.length)]);
+                int newDamageDone = (damageDone[random.nextInt(damageDone.length)]);
                 int calculatedDamage = damage + newDamageDone;
 
                 if (calculatedDamage >= 10) {
-                    lifeForce= lifeForce - 1; //subtract 1 from health
-                    calculatedDamage= 0; //new total set to 0
+                    lifeForce = lifeForce - 1; //subtract 1 from health
+                    calculatedDamage = 0; //new total set to 0
                 }
                 if (calculatedDamage < 10) {
                     damage = calculatedDamage; //sets damage to taken+damage
@@ -58,70 +62,72 @@ public class Ch10 {
             }
 
 
-
-            public void reaction () {
+            public void reaction() {
                 String[] sound = {"OW!", "Wham!", "POW!", "Boffo!!", "KaPow!", "That one hurt!"};
                 Random random = new Random();
                 System.out.println(sound[random.nextInt(sound.length)]);
             }
 
 
-
             public void reSpawn() { //sets lifeForce back to 10 hp
                 lifeForce = 10;
             }
-            public boolean isDead () {
+
+            public boolean isDead() {
                 if (lifeForce <= 0) return true;
                 else return false;
             }
-            public boolean isAlive () {
+
+            public boolean isAlive() {
                 if (lifeForce > 0) return true;
                 else return false;
             }
+
+            public void display() {
+                System.out.println("\n" + getNickName() + " status (health=" + getLifeForce() + ", damage=" + getDamage() + ")");
+                System.out.println("Archer is attacked and takes " + takenDamage() + " units of damage");
+                reaction();
+            }
+
+            public void fare() {
+                System.out.println("\n" + getNickName() + " status (health=" + getLifeForce() + ", damage=" + getDamage() + ")");
+            }
+
+
         }
 
+        WarriorFred[] arr;
+        arr = new WarriorFred[5];
 
+        arr[0] = new WarriorFred("Warrior Archer", 10, 0);
+        arr[1] = new WarriorFred("Warrior Mage", 10, 0);
+        arr[2] = new WarriorFred("Warrior Healer", 10, 0);
+        arr[3] = new WarriorFred("Warrior Tank", 10, 0);
+        arr[4] = new WarriorFred("Warrior Rouge", 10, 0);
 
-        WarriorFred archer = new WarriorFred("Warrior Archer",10,0); //new obj named Warrior Archer
-        WarriorFred mage = new WarriorFred("Warrior Mage",10,0);//new obj named Warrior Mage
-        WarriorFred healer = new WarriorFred("Warrior Healer",10,0);//new obj named Warrior Healer
-        WarriorFred tank = new WarriorFred("Warrior Tank",10,0);//new obj named Warrior Tank
-        WarriorFred rouge = new WarriorFred("Warrior Rouge",10,0);//new obj named Warrior Rouge
 
         System.out.println("*** Battle! ***   All warriors start with 10 units of health for 10 rounds");
 
         for (int i = 1; i < 11; i++) { // 10 rounds loops through
             System.out.println("\nRound: " + i);
 
-            System.out.println("\n" + archer.getNickName() + " status (health=" + archer.getLifeForce() + ", damage=" + archer.getDamage() + ")");
-            System.out.println("Archer is attacked and takes " + archer.takenDamage() + " units of damage");
-            archer.reaction();
+            arr[0].display();
+            arr[1].display();
+            arr[2].display();
+            arr[3].display();
+            arr[4].display();
 
-            System.out.println("\n" + mage.getNickName() + " status (health=" + mage.getLifeForce() + ", damage=" + mage.getDamage() + ")");
-            System.out.println("Mage is attacked and takes " + mage.takenDamage() + " units of damage");
-            mage.reaction();
-
-            System.out.println("\n" + healer.getNickName() + " status (health=" + healer.getLifeForce() + ", damage=" + healer.getDamage() + ")");
-            System.out.println("Healer is attacked and takes " + healer.takenDamage() + " units of damage");
-            healer.reaction();
-
-            System.out.println("\n" + tank.getNickName() + " status (health=" + tank.getLifeForce() + ", damage=" + tank.getDamage() + ")");
-            System.out.println("Tank is attacked and takes " + tank.takenDamage() + " units of damage");
-            tank.reaction();
-
-            System.out.println("\n" + rouge.getNickName() + " status (health=" + rouge.getLifeForce() + ", damage=" + rouge.getDamage() + ")");
-            System.out.println("Rouge is attacked and takes " + rouge.takenDamage() + " units of damage");
-            rouge.reaction();
-
-            if (i ==10){ //final status message for last round: Only prints on last round
-                System.out.println("\nHow did the warriors fare? ");
-                System.out.println("\n" + archer.getNickName() + " status (health=" + archer.getLifeForce() + ", damage=" + archer.getDamage() + ")");
-                System.out.println("\n" + mage.getNickName() + " status (health=" + mage.getLifeForce() + ", damage=" + mage.getDamage() + ")");
-                System.out.println("\n" + healer.getNickName() + " status (health=" + healer.getLifeForce() + ", damage=" + healer.getDamage() + ")");
-                System.out.println("\n" + tank.getNickName() + " status (health=" + tank.getLifeForce() + ", damage=" + tank.getDamage() + ")");
-                System.out.println("\n" + rouge.getNickName() + " status (health=" + rouge.getLifeForce() + ", damage=" + rouge.getDamage() + ")");
+            if (i == 10) { //final status message for last round: Only prints on last round
+                arr[0].fare();
+                arr[1].fare();
+                arr[2].fare();
+                arr[3].fare();
+                arr[4].fare();
             }
         }
-        }
-
     }
+}
+
+
+
+
